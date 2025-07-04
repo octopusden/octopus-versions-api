ad35c2fe33b4f32379d72e1bdf18edc9463037c8
fishinitself <mikhail.ershov@gmail.com>
2025-07-04 18:38:09 +0300

    mandatory isHotfixEnabled to ComponentVersionFormat::create

--- src/test/java/org/octopusden/releng/versions/ComponentVersionFormatTest.java
+++ src/test/java/org/octopusden/releng/versions/ComponentVersionFormatTest.java
@@ -8,15 +8,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ComponentVersionFormatTest {
 
     @Test
-    void testCreate() throws Exception {
+    void testCreateHF() throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         ComponentVersionFormat componentVersionFormat = ComponentVersionFormat.create("1", "2", "3", "4", "5", true);
         String componentVersionFormatString = objectMapper.writeValueAsString(componentVersionFormat);
         assertEquals(componentVersionFormat, objectMapper.readValue(componentVersionFormatString, ComponentVersionFormat.class));
+    }
 
-        componentVersionFormat = ComponentVersionFormat.create("1", "2", "3", "4", null, false);
-        componentVersionFormatString = objectMapper.writeValueAsString(componentVersionFormat);
+    @Test
+    void testCreateNoHF() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ComponentVersionFormat componentVersionFormat = ComponentVersionFormat.create("1", "2", "3", "4", null, false);
+        String componentVersionFormatString = objectMapper.writeValueAsString(componentVersionFormat);
         assertEquals(componentVersionFormat, objectMapper.readValue(componentVersionFormatString, ComponentVersionFormat.class));
-
     }
 }
